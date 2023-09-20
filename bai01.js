00e4f2d085ae41731e75d1cd0d820e088a791963
Son Lam <sonlam@Sons-MacBook-Air.local>
2023-09-20 19:44:03 +0700

    a

---
+++ bai01.js
@@ -0,0 +1,38 @@
+
+// console.log() 
+// cho in ra console
+
+// bai 1
+
+// câu a
+console.log('hello world')
+// câu b
+console.log('nguyen_canh_ky')
+// câu c
+console.clear()
+// câu d
+console.log('*****')
+console.log('*****')
+console.log('*****')
+console.log('*****')
+console.log('*****')
+// câu e
+
+console.log('     *     ')
+console.log('    ***    ')
+console.log('   *****   ')
+console.log('  *******  ')
+console.log(' ********* ')
+console.log('***********')
+// câu f
+console.log(' ** ** ')
+console.log('*  *  *')
+console.log('*     *')
+
+
+
+
+
+
+
+
