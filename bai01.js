75ee20c0e97cfb66458d402630098d8bbd449251
Son Lam <sonlam@Sons-MacBook-Air.local>
2023-09-22 19:30:04 +0700

    a

--- bai01.js
+++ bai01.js
@@ -9,7 +9,7 @@ console.log('hello world')
 // câu b
 console.log('nguyen_canh_ky')
 // câu c
-console.clear()
+// console.clear()
 // câu d
 console.log('*****')
 console.log('*****')
@@ -28,6 +28,47 @@ console.log('***********')
 console.log(' ** ** ')
 console.log('*  *  *')
 console.log('*     *')
+
+
+
+// bài 2
+
+// câu a
+let a=10
+console.log(a)
+
+// câu b
+b='10'
+console.log(b)
+
+// câu c
+a=20 
+console.log(a)
+
+// câu d
+const pi=3.14
+console.log(pi)
+
+// câu e
+
+
+console.clear()
+//  câu f
+ 
+let dien_thoai= {
+    ten: 'iphone14',
+    hangSanXuat: 'apple',
+    mausac: 'trang',
+    gia: '1000 usd'
+}
+dien_thoai.gia = '750 usd'
+dien_thoai.ten = 'iphone 14 promax'
+console.log(dien_thoai)
+
+
+
+
+
 
 
 
