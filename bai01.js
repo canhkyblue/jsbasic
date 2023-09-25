d840beb6aad94e635800f33ee03842d2c3889025
Son Lam <sonlam@Sons-MacBook-Air.local>
2023-09-25 19:42:28 +0700

    a

--- bai01.js
+++ bai01.js
@@ -1,69 +1,129 @@
 
-// console.log() 
-// cho in ra console
+// // console.log() 
+// // cho in ra console
 
-// bai 1
+// // bai 1
 
-// câu a
-console.log('hello world')
-// câu b
-console.log('nguyen_canh_ky')
-// câu c
+// // câu a
+// console.log('hello world')
+// // câu b
+// console.log('nguyen_canh_ky')
+// // câu c
+// // console.clear()
+// // câu d
+// console.log('*****')
+// console.log('*****')
+// console.log('*****')
+// console.log('*****')
+// console.log('*****')
+// // câu e
+
+// console.log('     *     ')
+// console.log('    ***    ')
+// console.log('   *****   ')
+// console.log('  *******  ')
+// console.log(' ********* ')
+// console.log('***********')
+// // câu f
+// console.log(' ** ** ')
+// console.log('*  *  *')
+// console.log('*     *')
+
+
+
+// // bài 2
+
+// // câu a
+// let a=10
+// console.log(a)
+
+// // câu b
+// b='10'
+// console.log(b)
+
+// // câu c
+// a=20 
+// console.log(a)
+
+// // câu d
+// const pi=3.14
+// console.log(pi)
+
+// // câu e
+
+
 // console.clear()
-// câu d
-console.log('*****')
-console.log('*****')
-console.log('*****')
-console.log('*****')
-console.log('*****')
-// câu e
-
-console.log('     *     ')
-console.log('    ***    ')
-console.log('   *****   ')
-console.log('  *******  ')
-console.log(' ********* ')
-console.log('***********')
-// câu f
-console.log(' ** ** ')
-console.log('*  *  *')
-console.log('*     *')
+// //  câu f
+ 
+// let dien_thoai= {
+//     ten: 'iphone14',
+//     hangSanXuat: 'apple',
+//     mausac: 'trang',
+//     gia: '1000 usd'
+// }
+// dien_thoai.gia = '750 usd'
+// dien_thoai.ten = 'iphone 14 promax'
+// console.log(dien_thoai)
 
 
-
-// bài 2
+// bài
 
 // câu a
-let a=10
-console.log(a)
 
-// câu b
-b='10'
-console.log(b)
+// let a=25
+// b=86
+// tong = a+b
+// hieu = a-b
+// tich = a*b
+// thuong = a/b
+// console.log('tong la',tong)
+// console.log('hieu la',hieu)
+// console.log('tich la',tich)
+// console.log('thuong la',thuong)
 
-// câu c
-a=20 
-console.log(a)
+// // câu d
+// let d=5
+// const pi=3.14
+// cv= d*pi
+// dt= (d/2)**2 *3.14
+// console.log('chu vi hinh tron',cv)
+// console.log('dien tich hinh tron',dt)
 
-// câu d
-const pi=3.14
-console.log(pi)
+// câu 5
 
-// câu e
+// let cd=15
+// cr= 10
+// cv= 2*(cd+cr)
+// dt= cd*cr
+// console.log(' chu vi hinh chu nhat la',cv)
+// console.log('dien tich hinh chu nhat la',dt)
+
+// // câu 6
+
+// let s= 120, t= 130/60
+// van_toc= s/t
+// console.log('van toc la',van_toc)
+
+// câu 7
+let t= 150/60
+let v=60
+let s=v*t
+console.log('quang duong la',s)
+
+// câu j
+ let v1=8*5*6
+ let v2= 8*3*5*6
+ let vtong= v1+v2
+ console.log('the tich khoi go la',vtong)
 
 
-console.clear()
-//  câu f
- 
-let dien_thoai= {
-    ten: 'iphone14',
-    hangSanXuat: 'apple',
-    mausac: 'trang',
-    gia: '1000 usd'
-}
-dien_thoai.gia = '750 usd'
-dien_thoai.ten = 'iphone 14 promax'
-console.log(dien_thoai)
+
+
+
+
+
+
+
 
 
 
---
+++ function.js
@@ -0,0 +1,9 @@
+
+
+function bieuthucnhan(a,b) {
+    const kq =a*b
+    return kq
+}
+ 
+const tinhnhan1 =(4,5)
+ console.log(tinhnhan1)
